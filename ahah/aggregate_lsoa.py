35417a32935f1f801cedf06822e55d3abc5fcbff
cjber <cjberragan@gmail.com>
2022-01-20 09:29:11 +0000

    aggregate with uprn and pcs

--- ahah/aggregate_lsoa.py
+++ ahah/aggregate_lsoa.py
@@ -1,5 +1,4 @@
-import dask_geopandas
-import geopandas as gpd
+import cudf
 import pandas as pd
 import re
 from ahah.common.utils import Config
@@ -7,50 +6,51 @@ from pathlib import Path
 from typing import Generator
 
 
-def read_dists(files: Generator, pc: Path) -> pd.DataFrame:
-    dists = pd.read_parquet(pc)
-    for file in files:
-        name = re.split(r"_|\.", file.name)[1]
-        dists = dists.merge(
-            pd.read_csv(file).rename(columns={"distance": name}),
-            on="postcode",
-        )
-    return dists
-
-
-def read_lsoa(lsoa_path: Path, dz_path: Path) -> gpd.GeoDataFrame:
-    lsoa = gpd.read_file(lsoa_path).filter(["code", "geometry"], axis=1)
-    dz = (
-        gpd.read_file(dz_path)
-        .filter(["DataZone", "geometry"], axis=1)
-        .rename(columns={"DataZone": "code"})
+def read_dists(dist_files: Generator, pc: cudf.DataFrame, uprn) -> pd.DataFrame:
+    dfs = cudf.concat(
+        [
+            cudf.read_csv(file)
+            .set_index("postcode")
+            .rename(columns={"distance": re.split(r"_|\.", file.name)[1]})
+            for file in dist_files
+        ],
+        axis=1,
     )
-    return lsoa.append(dz)
 
+    dfs = dfs.merge(uprn, on="postcode").dropna()
+    dfs["num_pc"] = 1
+    for poi in Config.POI_LIST:
+        dfs[poi] = dfs[poi] * dfs["uprn_count"]
 
-def aggregate_dists(dists: pd.DataFrame, zones: gpd.GeoDataFrame) -> pd.DataFrame:
-    gdf = gpd.GeoDataFrame(
-        dists, geometry=gpd.points_from_xy(dists["easting"], dists["northing"])
-    ).drop(["easting", "northing"], axis=1)
-    gdf.crs = zones.crs
-    gddf = dask_geopandas.from_geopandas(gdf, npartitions=16)
-    zones_dask = dask_geopandas.from_geopandas(zones, npartitions=16)
-    return (
-        dask_geopandas.sjoin(zones_dask, gddf)
-        .drop("index_right", axis=1)
-        .compute()
-        .groupby("code")
-        .mean()
-        .merge(zones, on="code")
-    )
+    dfs = dfs.groupby("lsoa11").sum()
+
+    for poi in Config.POI_LIST:
+        dfs[poi] = dfs[poi] / dfs["uprn_count"]
+
+    return dfs
+    # return (
+    #     pc.set_index("postcode")
+    #     .join(dfs, how="left")
+    #     .dropna()
+    #     .groupby("lsoa11")
+    #     .median()
+    # )
 
 
 if __name__ == "__main__":
-    files = Path(Config.OUT_DATA).glob("distances_*.csv")
-    pc_path = Path(Config.PROCESSED_DATA / "postcodes.parquet")
-    lsoa_path = Path(Config.RAW_DATA / "lsoa" / "england_lsoa_2011.shp")
-    dz_path = Path(Config.RAW_DATA / "lsoa" / "SG_DataZone_Bdry_2011.shp")
+    dist_files = list(Path(Config.OUT_DATA).glob("distances_*.csv"))
+    uprn = (
+        cudf.read_parquet(
+            Config.OUT_DATA / "uprn_pcs.parquet",
+        )
+        .rename(columns={"lsoa11cd": "lsoa11"})
+        .drop(["oa11cd"], axis=1)
+    )
 
-    dists = read_dists(files=files, pc=pc_path)
-    zones = read_lsoa(lsoa_path=lsoa_path, dz_path=dz_path)
-    joined = aggregate_dists(dists=dists, zones=zones)
+    pc = cudf.read_csv(
+        Config.RAW_DATA / "onspd" / "postcodes.csv",
+        usecols=["pcds", "lsoa11"],
+    ).rename(columns={"pcds": "postcode"})
+
+    dists = read_dists(dist_files, pc, uprn)
+    dists.to_csv(Config.OUT_DATA / "median_dists.csv")
