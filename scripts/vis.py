69d7a734f3737ee6a65637362d5c8a9ae2ca6506
cjber <cjberragan@gmail.com>
2024-08-14 15:19:17 +0100

    remove old dirs

--- scripts/vis.py
+++
@@ -1,9 +0,0 @@
-import geopandas as gpd
-import matplotlib.pyplot as plt
-import pandas as pd
-
-df = pd.read_csv("./data/out/distances_gpp.csv")
-df["distance_rank"] = df["distance"].rank()
-gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.easting, df.northing))
-gdf.plot(column="distance_rank", legend=True)
-plt.show()
